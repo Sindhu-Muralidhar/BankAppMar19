ab11a878d52625131864e52a50ba4120cd32f333
Sindhu Muralidhar <sindhu.comp@gmail.com>
2019-05-01 19:26:07 -0700

    Commiting 2

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -4,13 +4,55 @@ using System.Text;
 
 namespace BankApp
 {
+    /// <summary>
+    /// Account that represents bank account.
+    /// here you can withdraw or deposit money
+    /// </summary>
     class Account
 
     {
+        #region Properties
+        /// <summary>
+        /// Unique number asscociatd with a customer for their account
+        /// </summary>
         public int AccountNumber { get; set; }
+        /// <summary>
+        /// Email address of the account holder
+        /// </summary>
         public string EmailAddress { get; set; }
+        /// <summary>
+        /// Available balance
+        /// </summary>
         public decimal Balance { get; set; }
+        /// <summary>
+        /// Type of account
+        /// </summary>
         public string AccountType { get; set; }
+        /// <summary>
+        /// Date when the account was created 
+        /// </summary>
         public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Methods 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount">Amount to be deposited </param>
+
+    public void Deposit(decimal amount)
+        {
+            //Balance = Balance + amount
+            Balance += amount;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount">amount to be withdrawn</param>
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,7 +6,12 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a1 = new Account();
+            //"new" word allows us to create an instance.So basically a1 
+            //is an instance of the class Account.
+            a1.AccountNumber = 123456;
+            a1.Balance = 1000;
+
         }
     }
 }
